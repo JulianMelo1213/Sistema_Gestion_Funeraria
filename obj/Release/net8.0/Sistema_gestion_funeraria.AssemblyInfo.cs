095443b6c92b9ceba81fd160f780d3aa67b026a7
GuilleAQN <omarguillermo1@gmail.com>
2024-06-29 18:16:58 -0400

    feat: Eliminar codigo generado boilerplate

--- obj/Release/net8.0/Sistema_gestion_funeraria.AssemblyInfo.cs
+++ obj/Release/net8.0/Sistema_gestion_funeraria.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_gestion_funeraria")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b60aa047a35e21bf513e2e24973ff77e301863")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_gestion_funeraria")]
