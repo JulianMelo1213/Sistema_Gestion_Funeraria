aceb6c51f0dedc10829d851a4e1bc364405ee766
JulianMelo1213 <gp20-1333@unphu.edu.do>
2024-06-30 14:44:22 -0400

    Cambios

--- obj/Debug/net8.0/Sistema_gestion_funeraria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema_gestion_funeraria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_gestion_funeraria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3b18cc3fd49c0b6f644fd3dbe97bd3d5a4aa0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7caa3235954291661cccb32707068ca6cc0b5a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_gestion_funeraria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_gestion_funeraria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
