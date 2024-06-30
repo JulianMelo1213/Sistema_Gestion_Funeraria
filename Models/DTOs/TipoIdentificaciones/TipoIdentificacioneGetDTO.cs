c20cade06d92b75534c4bfa5d75a0d1522218925
EmillAPU <ep20-1420@unphu.edu.do>
2024-06-30 16:29:03 -0400

    Feat: Incluir DTO y arreglar controlador de tipoIdentificaciones

---
+++ Models/DTOs/TipoIdentificaciones/TipoIdentificacioneGetDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sistema_gestion_funeraria.Models.DTOs.TipoIdentificaciones
+{
+    public class TipoIdentificacioneGetDTO
+    {
+        public int IdTipoIdentificacion { get; set; }
+
+        public string Nombre { get; set; } = null!;
+    }
+}
---
+++ Models/DTOs/TipoIdentificaciones/TipoIdentificacioneUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_gestion_funeraria.Models.DTOs.TipoIdentificaciones
+{
+    public class TipoIdentificacioneUpdateDTO
+    {
+        [Required(ErrorMessage = "Id es requerido")]
+        public int IdTipoIdentificacion { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "Nombre no puede ser mayor a 100 carácteres")]
+        public string Nombre { get; set; } = null!;
+    }
+}
